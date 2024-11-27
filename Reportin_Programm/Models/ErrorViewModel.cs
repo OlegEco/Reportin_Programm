8897a36e1c0edb87f2af2c9bd565e75fb119a4bf
OlegEco <stalkerkrutoj@gmail.com>
2024-11-27 18:54:07 +0100

    Add project files.

---
+++ Reportin_Programm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reportin_Programm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
